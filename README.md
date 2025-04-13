f22457b07600ce58abc3296b8d0522d528b37bfb
jayur830 <jayur830@naver.com>
2025-04-13 20:19:33 +0900

    ci: 레포지토리 계정 이관, 스크립트 수정

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Commit Convention 자동 설치 툴킷
+# Commit Convention Auto-Install Toolkit
 
-프로젝트 세팅할 때마다 `husky`, `commitlint`, `commitizen` 등등등 맨날 수동으로 `npm i -D 어쩌구 ...` 하면서 깔았었는데 이게 너무 귀찮아서 그냥 모듈로 만들었습니다.  
+I got tired of manually installing `husky`, `commitlint`, `commitizen`, etc. with `npm i -D blah blah ...` every time I set up a new project, so I created this module.
 
 ## Usage
 
-사용법은 아래와 같이 하시면 됩니다.
+You can use it as follows:
 
 ```bash
 npx create-commit-convention   # npm
@@ -12,8 +12,13 @@ yarn create commit-convention  # yarn
 pnpm create commit-convention  # pnpm
 ```
 
-위 명령을 실행하시면 본인의 프로젝트에 `.cz-config.js`, `commitlint.config.js`, `.husky` 디렉토리, `.husky` 디렉토리 밑에 `commit-msg`, `prepare-commit-msg`라는 파일이 생길껍니다.  
-그리고 `package.json`의 `scripts`에 `prepare`라는 스크립트가 추가되고, `config`라는 필드에 commitizen 관련 설정이 추가되어 있을껍니다.
+When you run the above command, the following files will be created in your project:
 
-그리고 `.git` 디렉토리가 프로젝트에 있어야 하는데 없으면 자동으로 `git init` 합니다.
+- `.cz-config.js`
+- `commitlint.config.js`
+- `.husky` directory
+- `commit-msg` and `prepare-commit-msg` files under the `.husky` directory
 
+Additionally, a `prepare` script will be added to the `scripts` section of your `package.json`, and commitizen-related configurations will be added to the `config` field.
+
+If there is no `.git` directory in your project, it will automatically run `git init`.
--- run.sh
+++ run.sh
@@ -1,103 +1,56 @@
+#!/bin/bash
+
+# Initialize git repository
 if [ ! -d '.git' ]; then
-  git init
+  echo git init
 fi
-if [ -d yarn.lock ]; then
-  yarn add -D \
-    husky \
-    @commitlint/cli \
-    @commitlint/config-conventional \
-    commitizen \
-    cz-customizable
+
+# Initialize packages if package.json doesn't exist
+if [ ! -e 'package.json' ]; then
+  npm init -y
+  npm i
+fi
+
+# For yarn users
+if [ -e yarn.lock ]; then
+  yarn add -D husky commitizen @commitlint/cli @commitlint/config-conventional
   yarn husky
-elif [ -d pnpm-lock.yaml ]; then
-  pnpm add -D \
-    husky \
-    @commitlint/cli \
-    @commitlint/config-conventional \
-    commitizen \
-    cz-customizable
-  pnpm dlx husky
-else
-  npm i -D \
-    husky \
-    @commitlint/cli \
-    @commitlint/config-conventional \
-    commitizen \
-    cz-customizable
+  yarn commitizen init cz-conventional-changelog --yarn --dev --exact
+
+# For pnpm users
+elif [ -e pnpm-lock.yaml ]; then
+  pnpm add -D husky commitizen @commitlint/cli @commitlint/config-conventional
+  pnpm husky
+  pnpm commitizen init cz-conventional-changelog --pnpm --save-dev --save-exact
+
+# For npm users
+elif [ -e package-lock.json ]; then
+  npm i -D husky commitizen @commitlint/cli @commitlint/config-conventional
   npx husky
+  npx commitizen init cz-conventional-changelog --save-dev --save-exact
+
 fi
 
-echo "module.exports = {
-  extends: ['@commitlint/config-conventional'],
-};" > commitlint.config.js
-
-echo "module.exports = {
-  types: [
-    {
-      value: 'feat',
-      name: 'feat:\t\t신규 기능 개발로 인한 코드 변경 사항',
-    },
-    {
-      value: 'fix',
-      name: 'fix:\t\t버그 개선으로 인한 코드 변경 사항',
-    },
-    {
-      value: 'docs',
-      name: 'docs:\t\tmd 파일 및 기타 문서 변경 사항',
-    },
-    {
-      value: 'style',
-      name: 'style:\t코드 포맷팅, 세미콜론 누락 등 코드 정리에 의한 변경 사항',
-    },
-    { value: 'refactor', name: 'refactor:\t코드 리팩토링 변경 사항' },
-    {
-      value: 'test',
-      name: 'test:\t\t테스트 코드 작성 및 수정에 대한 변경 사항',
-    },
-    {
-      value: 'chore',
-      name: 'chore:\t패키지 관련 설정 변경 사항',
-    },
-    {
-      value: 'perf',
-      name: 'perf:\t\t성능 개선으로 인한 코드 변경 사항',
-    },
-    {
-      value: 'revert',
-      name: 'revert:\t롤백에 의한 변경 사항',
-    },
-    {
-      value: 'build',
-      name: 'build:\t번들러 설정 또는 라이브러리 버전 등 빌드에 영향이 있는 변경 사항',
-    },
-    {
-      value: 'ci',
-      name: 'ci:\t\t배포 관련 구성파일 변경 사항',
-    },
-  ],
-  scopes: [''],
-  allowCustomScopes: true,
-  subjectLimit: 255,
-  allowBreakingChanges: ['feat', 'fix', 'refactor', 'chore', 'perf', 'revert', 'build'],
-  skipQuestions: ['body'],
-};" > .cz-config.js
-
-if [ -d '.husky' ]; then
-  if [ -d yarn.lock ]; then
-    echo '#!/usr/bin/env sh\n. "$(dirname -- "$0")/_/husky.sh"\n\nyarn commitlint --edit $1' > .husky/commit-msg
-    echo '#!/usr/bin/env sh\n. "$(dirname -- "$0")/_/husky.sh"\n\nexec < /dev/tty && yarn cz --hook || true' > .husky/prepare-commit-msg
-  elif [ -d pnpm-lock.yaml ]; then
-    echo '#!/usr/bin/env sh\n. "$(dirname -- "$0")/_/husky.sh"\n\npnpm commitlint --edit $1' > .husky/commit-msg
-    echo '#!/usr/bin/env sh\n. "$(dirname -- "$0")/_/husky.sh"\n\nexec < /dev/tty && pnpm cz --hook || true' > .husky/prepare-commit-msg
-  else
-    echo '#!/usr/bin/env sh\n. "$(dirname -- "$0")/_/husky.sh"\n\nnpx commitlint --edit $1' > .husky/commit-msg
-    echo '#!/usr/bin/env sh\n. "$(dirname -- "$0")/_/husky.sh"\n\nexec < /dev/tty && npx cz --hook || true' > .husky/prepare-commit-msg
-  fi
-fi
-
+# Configure package.json
 if [ `grep -c '"prepare": "husky"' package.json` == 0 ]; then
   sed -i '' 's/  "scripts": {/  "scripts": {\n    "prepare": "husky",/g' package.json
 fi
-if [ `grep -c '"path": "node_modules/cz-customizable"' package.json` == 0 ]; then
-  sed -i '' 's/  }$/  },\n  "config": {\n    "commitizen": {\n      "path": "node_modules\/cz-customizable"\n    }\n  }/g' package.json
-fi
+
+# Create prepare-commit-msg file
+echo "#!/bin/bash
+exec < /dev/tty && node_modules/.bin/cz --hook || true
+" > .husky/prepare-commit-msg
+
+chmod +x .husky/prepare-commit-msg
+
+# Configure commitlint
+echo "module.exports = { extends: ['@commitlint/config-conventional'] };" > commitlint.config.js
+
+echo "#!/bin/bash
+node_modules/.bin/commitlint --edit "$1"
+" > .husky/commit-msg
+
+chmod +x .husky/commit-msg
+
+# Husky Install
+yarn husky
